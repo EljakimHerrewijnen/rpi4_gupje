d812903514d1168a50163866277dddd193062738
Eljakim Herrewijnen <eljakim.herrewijnen@gmail.com>
2025-03-21 10:03:53 +0100

    Example code for stepping

--- qemu.py
+++ qemu.py
@@ -1,12 +1,10 @@
 import serial, time
-from typing import cast
 import subprocess
 from keystone import *
 from ghidra_assistant.concrete_device import ConcreteDevice
 from ghidra_assistant.utils.debugger.debugger_archs.ga_arm64 import GA_arm64_debugger
 from qiling.const import *
 from ghidra_assistant.utils.utils import *
-from ghidra_assistant.utils.archs.arm64.arm64_stepper import ARM64Stepper
 
 ks = Ks(KS_ARCH_ARM64, KS_MODE_LITTLE_ENDIAN)
 
@@ -30,10 +28,10 @@ while True:
     if b"SEND DEBUGGER" in data:
         break
     
-# UART_SEND = data.decode().split("\n")[0].strip().split(": ")[1]
-# UART_RECV = data.decode().split("\n")[1].strip().split(": ")[1]
+UART_SEND = data.decode().split("\n")[0].strip().split(": ")[1]
+UART_RECV = data.decode().split("\n")[1].strip().split(": ")[1]
 
-DEBUGGER_PATH = "../../bin/rpi4/debugger.bin"
+DEBUGGER_PATH = "../../../bin/rpi4/debugger.bin"
 debugger = open(DEBUGGER_PATH, "rb").read()
 
 # Test shellcode
@@ -93,48 +91,22 @@ class RaspberryPi4():
     def setup_concrete_device(self, concrete_device : ConcreteDevice):
         #Setup architecture
         concrete_device.arch = QL_ARCH.ARM64
-        concrete_device.ga_debugger_location = 0x81000  # TODO, not used yet
-        concrete_device.ga_vbar_location = 0x81000 + 0x1000
-        concrete_device.ga_storage_location = 0x85000
-        concrete_device.ga_stack_location = 0x83000
+        concrete_device.ga_debugger_location = 0x2069000  # TODO, not used yet
+        concrete_device.ga_vbar_location = 0x206d000 + 0x1000
+        concrete_device.ga_storage_location = 0x206d000
+        concrete_device.ga_stack_location = 0x206b000
         
         concrete_device.arch_dbg = GA_arm64_debugger(concrete_device.ga_vbar_location, concrete_device.ga_debugger_location, concrete_device.ga_storage_location)
         concrete_device.arch_dbg.read = self.read
         concrete_device.arch_dbg.write = self.write
-        
 
         #Overwrite all calls to make the concrete target function properly
         concrete_device.copy_functions()
-        
-        return concrete_device
 
 pi4 = RaspberryPi4(ser)
 cd = ConcreteDevice(None, False)
-cd = pi4.setup_concrete_device(cd)
-
-cast(GA_arm64_debugger, cd.arch_dbg) # type hinting
-
+pi4.setup_concrete_device(cd)
 cd.memdump_region(0x80000, 0x100)
 cd.memdump_region(0x80000, 0x2000) # TODO fix big chunks
 cd.memwrite_region(0x10000, b"\xee" * 0x10)
-
-CODE_CAVE = 0x80000 + 0x10000
-
-SHELLCODE = """
-    mov x0, 0x10000
-    NOP
-    NOP
-    mov x1, 0x10
-    cmp x0, x1
-    bne 0x84000
-    ret
-"""
-cd.memwrite_region(CODE_CAVE, ks.asm(SHELLCODE, as_bytes=True)[0])
-
-# cd.restore_stack_and_jump(cd.arch_dbg.debugger_addr)
-# cd.read(4) == b"GiAs"
-
-
-stepper = ARM64Stepper(cd, CODE_CAVE, False)
-stepper.run(stepper.pc)
 pass
